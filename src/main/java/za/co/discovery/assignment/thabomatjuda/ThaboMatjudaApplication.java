e686f2936dd1810b8ad4448cb8a99f49f0573cdb
Thabo Matjuda <tl.matjuda@gmail.com>
2023-11-14 13:33:09 +0200

    Initial commit

---
+++ src/main/java/za/co/discovery/assignment/thabomatjuda/ThaboMatjudaApplication.java
@@ -0,0 +1,13 @@
+package za.co.discovery.assignment.thabomatjuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaboMatjudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaboMatjudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/discovery/assignment/thabomatjuda/ThaboMatjudaApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.discovery.assignment.thabomatjuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaboMatjudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
