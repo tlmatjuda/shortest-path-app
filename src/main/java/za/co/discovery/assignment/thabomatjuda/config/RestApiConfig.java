a4efa2139a64b24ffbcb9d0501da826aa518d524
Thabo Matjuda <tl.matjuda@gmail.com>
2023-11-19 16:08:36 +0200

    #3.  Expose the database using a RESTful Webservice. Swagger in place now.

---
+++ src/main/java/za/co/discovery/assignment/thabomatjuda/config/RestApiConfig.java
@@ -0,0 +1,15 @@
+package za.co.discovery.assignment.thabomatjuda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class RestApiConfig {
+
+}
