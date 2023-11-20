c2f26e05a6319dfea6d48aa681a667236e2758f1
Thabo Matjuda <tl.matjuda@gmail.com>
2023-11-20 18:30:14 +0200

    #5. Expose the algorithm using a Document Literal Webservice. Cleaning up the Request & Response

--- src/main/java/za/co/discovery/assignment/thabomatjuda/ThaboMatjudaApplication.java
+++ src/main/java/za/co/discovery/assignment/thabomatjuda/ThaboMatjudaApplication.java
@@ -3,6 +3,7 @@ package za.co.discovery.assignment.thabomatjuda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ThaboMatjudaApplication {
 
--- src/main/java/za/co/discovery/assignment/thabomatjuda/soap/endpoint/ShortestPathEndpoint.java
+++ src/main/java/za/co/discovery/assignment/thabomatjuda/soap/endpoint/ShortestPathEndpoint.java
@@ -1,7 +1,6 @@
 package za.co.discovery.assignment.thabomatjuda.soap.endpoint;
 
 
-import jakarta.xml.bind.JAXBElement;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -11,23 +10,22 @@ import za.co.discovery.assignment.thabomatjuda.config.SoapWsConfig;
 import za.co.discovery.assignment.thabomatjuda.soap.gen.GetShortestPathRequest;
 import za.co.discovery.assignment.thabomatjuda.soap.gen.GetShortestPathResponse;
 
-import javax.xml.namespace.QName;
-
 @Endpoint
 @Component
 public class ShortestPathEndpoint {
 
     @PayloadRoot( namespace = SoapWsConfig.NAMESPACE_URI, localPart = "GetShortestPathRequest")
     @ResponsePayload
-    public JAXBElement<GetShortestPathResponse> getShortestPath(@RequestPayload JAXBElement<GetShortestPathRequest> request) {
+    public GetShortestPathResponse getShortestPath(@RequestPayload GetShortestPathRequest request) {
         GetShortestPathResponse response = new GetShortestPathResponse();
         response.setTotaldistance(2.38D);
 
-        return createJaxbElement(response, GetShortestPathResponse.class);
+//        return createJaxbElement(response, GetShortestPathResponse.class);
+        return response;
     }
 
-    private <T> JAXBElement<T> createJaxbElement(T object, Class<T> clazz) {
-        return new JAXBElement<>(new QName(clazz.getSimpleName()), clazz, object);
-    }
+//    private <T> JAXBElement<T> createJaxbElement(T object, Class<T> clazz) {
+//        return new JAXBElement<>(new QName(clazz.getSimpleName()), clazz, object);
+//    }
 
 }
