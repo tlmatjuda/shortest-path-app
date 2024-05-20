2a3454400664e5c440b2201f4efb91150079bfee
Thabo Matjuda <tl.matjuda@gmail.com>
2024-05-20 19:34:40 +0200

    Planet Query Service testing.

--- src/test/java/com/toob/service/shortest/service/PlanetQueryServiceTest.java
+++ src/test/java/com/toob/service/shortest/service/PlanetQueryServiceTest.java
@@ -1,37 +1,64 @@
 package com.toob.service.shortest.service;
 
+import com.google.common.collect.Lists;
+import com.toob.service.shortest.entity.Planet;
 import com.toob.service.shortest.model.planet.PlanetModel;
+import com.toob.service.shortest.rest.AbstractServiceTest;
 import com.toob.service.shortest.service.PlanetQueryService;
+import lombok.SneakyThrows;
 import org.apache.commons.collections4.CollectionUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
-class PlanetQueryServiceTest {
+class PlanetQueryServiceTest extends AbstractServiceTest {
 
     @Autowired
     private PlanetQueryService planetQueryService;
 
+    Planet earth;
+    Planet moon;
+    Planet jupiter;
+    Planet venus;
 
-
-    @Test
-    void shouldFetchAll() throws Exception {
-        List<PlanetModel> planetModels = planetQueryService.fetchAll();
-        assertTrue( CollectionUtils.isNotEmpty( planetModels));
+    @BeforeEach
+    void setUp() {
+        earth = mockPlanet("A", "Earth");
+        moon = mockPlanet("B", "Moon");
+        jupiter = mockPlanet("C", "Jupiter");
+        venus = mockPlanet("D", "Venus");
     }
 
     @Test
-    void shouldFetchById() throws Exception {
-        PlanetModel foundRecord = planetQueryService.fetchById("C");
+    @SneakyThrows
+    void shouldFetchAll() {
+        ArrayList<Planet> planets = Lists.newArrayList(earth, moon, jupiter, venus);
+        when( planetRepository.findAll()).thenReturn(planets);
+
+        List<PlanetModel> planetModels = planetQueryService.fetchAll();
+        assertTrue( CollectionUtils.isNotEmpty( planetModels));
+        verify (planetRepository, atLeastOnce()).findAll();
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldFetchById() {
+        when( planetRepository.findById( moon.getNode())).thenReturn( Optional.of(moon));
+        PlanetModel foundRecord = planetQueryService.fetchById( moon.getNode());
         assertNotNull( foundRecord);
         assertNotNull( foundRecord.getName());
-        assertEquals( "Jupiter", foundRecord.getName());
+        assertEquals( moon.getName(), foundRecord.getName());
+        verify (planetRepository, atLeastOnce()).findById( moon.getNode());
     }
 
 }
