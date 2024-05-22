7cd7faa413392954d8ab59cafae912e51b4ec3a2
Thabo Matjuda <tl.matjuda@gmail.com>
2024-05-22 17:52:17 +0200

    Fixing up a test case for the Planet Repository.

--- src/test/java/com/toob/service/shortest/repository/PlanetRepositoryTest.java
+++ src/test/java/com/toob/service/shortest/repository/PlanetRepositoryTest.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @Slf4j
+@SpringBootTest
 class PlanetRepositoryTest extends AbstractRepositoryTest {
 
     @BeforeAll
