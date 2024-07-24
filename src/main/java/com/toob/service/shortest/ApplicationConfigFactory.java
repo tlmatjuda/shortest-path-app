f2c04fc34c48f7356d5e6c2ad1aaf5638296234e
Thabo Matjuda <tl.matjuda@gmail.com>
2024-07-24 10:43:34 +0200

    Implemented Docker Compose.

--- src/main/java/com/toob/service/shortest/ApplicationConfigFactory.java
+++ src/main/java/com/toob/service/shortest/ApplicationConfigFactory.java
@@ -1,10 +1,7 @@
 package com.toob.service.shortest;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -13,23 +10,4 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @Configuration
 @EnableWebMvc
-public class ApplicationConfigFactory {
-
-    private final Environment environment;
-
-    public ApplicationConfigFactory(Environment environment) {
-        this.environment = environment;
-    }
-
-    @Bean( initMethod = "migrate")
-    public Flyway flyway() {
-        String dbUrl = environment.getRequiredProperty("spring.datasource.url");
-        String dbUserName = environment.getRequiredProperty("spring.datasource.username");
-        String dbUserPassword = environment.getRequiredProperty("spring.datasource.password");
-        return new Flyway( Flyway.configure()
-                .baselineOnMigrate(true)
-                .dataSource(dbUrl, dbUserName, dbUserPassword)
-                .locations("classpath:database/migration"));
-    }
-
-}
+public class ApplicationConfigFactory {}
